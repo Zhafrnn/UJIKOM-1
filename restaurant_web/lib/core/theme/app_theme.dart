738abfaa0df8ea36c95b1007bd090ece527d5e83
Zhafrnn <haydarzhafran3@gmail.com>
2026-02-10 09:05:11 +0700

    Day-2

--- restaurant_web/lib/core/theme/app_theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData appTheme() {
-  return ThemeData(
-    scaffoldBackgroundColor: Colors.white,
-    primaryColor: Colors.orange,
-    useMaterial3: true,
-  );
-}
